bc12635debe0df7c39c0114bd482590e3eb79ffd
gstoutt-dot <gstoutt@gmail.com>
2026-04-10 22:03:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function HomePage() {
     { label: "Home", href: "/" },
     { label: "Services", href: "/services" },
     { label: "Why Switch", href: "/why-switch" },
-    { label: "Founder", href: "/https://glennstoutt.com" },
+    { label: "Founder", href: "/glennstoutt.com" },
     { label: "Coverage", href: "/coverage" },
     { label: "Proposal", href: "/proposal" },
   ];
