64d7142068a8eba4f9f7f690916be6b7c15da414
gstoutt-dot <gstoutt@gmail.com>
2026-04-16 18:12:15 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -389,10 +389,10 @@ className="inline-flex items-center justify-center rounded-full border border-wh
 
       <div className="overflow-hidden rounded-[2rem] border border-white/10 shadow-[0_0_40px_rgba(0,0,0,0.4)]">
         <img
-          src="/condo.jpg"
-          alt="Condominium community aerial view"
-          className="h-[420px] w-full object-cover object-center transition duration-700 hover:scale-105"
-        />
+  src="/condo.jpeg"
+  alt="Condominium community aerial view"
+  className="h-[420px] w-full object-cover object-center transition duration-700 hover:scale-105"
+/>
       </div>
     </div>
 
