c3870ab8a6d2506ac0e602364c670329c0b0cb83
gstoutt-dot <gstoutt@gmail.com>
2026-04-30 00:27:54 -0400

    Update action-items.js

--- pages/board/action-items.js
+++ pages/board/action-items.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Link from "next/link";
+import Link from "next/link"; 
 import { supabase, createBOSAction } from "../../lib/bosClient";
 
 export default function ActionItems() {
