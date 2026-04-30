d0a8c719d2137c3eae5ac25ac84e32090ba9fac0
gstoutt-dot <gstoutt@gmail.com>
2026-04-30 01:17:55 -0400

    Update workflow-engine.js

--- pages/board/workflow-engine.js
+++ pages/board/workflow-engine.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { supabase } from "../../lib/bosClient";
+import { supabase } from "../../lib/bosClient"; 
 
 export default function WorkflowEngine() {
   const [actions, setActions] = useState([]);
