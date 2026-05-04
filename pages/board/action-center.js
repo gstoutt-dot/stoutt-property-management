abff552e03360d406c2af693de34a42980c896c7
gstoutt-dot <gstoutt@gmail.com>
2026-05-04 19:18:30 -0400

    Update action-center.js

--- pages/board/action-center.js
+++ pages/board/action-center.js
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 const BOARD_DECISION_STATUSES = ["board_review", "approved", "rejected"];
 
