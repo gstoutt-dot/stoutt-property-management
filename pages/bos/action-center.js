9ffc4ff7004f8157307098f68fb9d96f2158f6ca
gstoutt-dot <gstoutt@gmail.com>
2026-05-10 16:19:59 -0400

    Update action-center.js

--- pages/bos/action-center.js
+++ pages/bos/action-center.js
@@ -1052,6 +1052,7 @@ function buildFallbackPayload(workflowAction) {
 function getNotificationEventType(workflowAction) {
   const map = {
     manager_verified: "manager_review",
+    accounting_review: "manager_review",
     send_to_board: "board_review",
     request_clarification: "manager_review",
     dispatch_vendor: "vendor_dispatched",
