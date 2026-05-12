8562210bb5879bee8c90c1ea23f60b184aba432b
gstoutt-dot <gstoutt@gmail.com>
2026-05-12 12:39:18 -0400

    Update index.js

--- pages/homeowner/index.js
+++ pages/homeowner/index.js
@@ -230,8 +230,8 @@ const [loadError, setLoadError] = useState("");
                 </div>
               ))}
             </div>
-          
-        }
+         </>
+        )}
       </section>
 
       <section className="mx-auto grid max-w-7xl gap-6 px-6 pb-12 lg:grid-cols-3">
