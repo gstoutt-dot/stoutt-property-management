8247c24bae935ca2c382f77f4522d2f2bad47027
gstoutt-dot <gstoutt@gmail.com>
2026-05-20 12:31:29 -0400

    Fix import statement formatting in reports.js

--- pages/portal/board/reports.js
+++ pages/portal/board/reports.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react"; 
 import Link from "next/link";
 import { supabase } from "../../../lib/supabaseClient";
 
