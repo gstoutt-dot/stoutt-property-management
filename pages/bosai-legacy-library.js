e907b0355a8bdf14adad99f4d01244c919100feb
gstoutt-dot <gstoutt@gmail.com>
2026-06-08 12:08:16 -0400

    Add BosaiLegacyLibraryPage component

--- pages/bosai-legacy-library.js
+++ pages/bosai-legacy-library.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function BosaiLegacyLibraryPage() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#020617", color: "white", padding: "80px" }}>
+      <h1>The BOSai℠ Legacy Library</h1>
+      <p>Five volumes. One philosophy. Leadership before technology.</p>
+    </div>
+  );
+}
