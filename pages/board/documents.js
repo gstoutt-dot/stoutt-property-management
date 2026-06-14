7e6a718d3340f0efeae71e6eb68a9a72efa00f35
gstoutt-dot <gstoutt@gmail.com>
2026-06-13 23:07:29 -0400

    Update documents.js

--- pages/board/documents.js
+++ pages/board/documents.js
@@ -1,7 +1,10 @@
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 
-const DEFAULT_ASSOCIATION_ID = "622aaf96-ae1c-4f98-b0b2-00cc9178c2a2";
+const DEFAULT_ASSOCIATION_ID =
+  typeof window !== "undefined"
+    ? localStorage.getItem("spm_selected_association_id") || ""
+    : "";
 
 export default function Documents() {
   const [documents, setDocuments] = useState([]);
