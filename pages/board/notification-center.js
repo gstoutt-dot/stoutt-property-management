c4379450e3066c14a2d823be5da165686fc5d167
gstoutt-dot <gstoutt@gmail.com>
2026-06-15 11:21:27 -0400

    Fix import statement for supabase in notification-center.js

--- pages/board/notification-center.js
+++ pages/board/notification-center.js
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { supabase } from "../../lib/bosClient";
+import { supabase } from "../../lib/bosClient"; 
 
 const NOTIFICATION_SOURCE = "board_notification_center";
 
