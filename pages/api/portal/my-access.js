43357525725025cddddbf547951a0d9d5ee08cb1
gstoutt-dot <gstoutt@gmail.com>
2026-06-17 08:51:08 -0400

    Add my-access API endpoint with user data

---
+++ pages/api/portal/my-access.js
@@ -0,0 +1,12 @@
+{
+  success: true,
+  email: "cam@example.com",
+  roles: ["manager"],
+  associations: [
+    {
+      id: "79893883-6141-4dcc-ba1a-034d70a0dc96",
+      name: "Sunset Condo Association",
+      role: "manager"
+    }
+  ]
+}
