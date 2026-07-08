f7d5fa76bfa7d9061a6fbffb699c50017d63b7f8
gstoutt-dot <gstoutt@gmail.com>
2026-07-08 14:56:13 -0400

    Fix import statement formatting in request-proposal.js

--- pages/api/request-proposal.js
+++ pages/api/request-proposal.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"; 
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 const supabaseAdmin = createClient(
